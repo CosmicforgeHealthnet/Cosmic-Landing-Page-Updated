6b918f43609e66f626d7d4b3d757e56e3dcc343a
COSMIC <cosmicforge.dev.team@gmail.com>
2025-08-19 11:25:26 +0100

    df

--- src/providers/AOSProvider.jsx
+++ src/providers/AOSProvider.jsx
@@ -2,8 +2,8 @@
 "use client";
 
 import { useEffect } from "react";
+
 import AOS from "aos";
-import "aos/dist/aos.css";
 
 export default function AOSProvider({ children }) {
    useEffect(() => {
